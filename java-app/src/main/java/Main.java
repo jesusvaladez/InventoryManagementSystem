7016229018624e7eef6b74a81a406ce73ef9fdf5
Jesus Valadez <valadez2904@gmail.com>
2025-06-05 19:43:09 -0500

    code that dont work

--- java-app/src/main/java/Main.java
+++ java-app/src/main/java/Main.java
@@ -1,7 +1,25 @@
 package main.java;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
 
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        } catch (UnsupportedLookAndFeelException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        UIManager.LookAndFeelInfo[] looks = UIManager.getInstalledLookAndFeels();
+            for (UIManager.LookAndFeelInfo look : looks) {
+                System.out.println(look.getClassName());
+            }
     }
 }
