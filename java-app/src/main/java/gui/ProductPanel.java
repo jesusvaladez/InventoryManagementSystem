bd050eb0345b3bba524308e099cc67dfc88ec80c
Jesus Valadez <valadez2904@gmail.com>
2025-06-17 23:14:55 -0500

    more changes to ProductPanel class

--- java-app/src/main/java/gui/ProductPanel.java
+++ java-app/src/main/java/gui/ProductPanel.java
@@ -1,10 +1,13 @@
 package main.java.gui;
 
 import main.java.api.ProductApiClient;
+import main.java.model.Product;
 
 import javax.swing.*;
 import javax.swing.table.AbstractTableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Purpose: Will display products in a table and deal w CRUD operations
@@ -58,12 +61,82 @@ public class ProductPanel extends JPanel {
     }
 
     private JPanel createButtonPanel() {
-        return null;
+        JPanel panel = new JPanel(new FlowLayout());
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        panel.add(refreshButton);
+        panel.add(addButton);
+        panel.add(editButton);
+        panel.add(deleteButton);
+
+        return panel;
     }
 
+    /**
+     * Event handlers for all components that have actions
+     */
     private void setupEventHandlers() {
+        // Refresh
+        refreshButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                loadProducts();
+            }
+        });
+
+        // Add
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addProductMessage();
+            }
+        });
+
+        // Edit
+        editButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                editProductMessage();
+            }
+        });
+
+        // Delete
+        deleteButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        // Allows buttons
+        productTable.getSelectionModel().addListSelectionListener(e -> {
+            boolean hasSelection = productTable.getSelectedRow() != -1;
+            editButton.setEnabled(hasSelection);
+            deleteButton.setEnabled(hasSelection);
+        });
+    }
+
+    private void addProductMessage() {
+        // TODO: Make this actually add a product
+        // Placeholder for now
+        JOptionPane.showMessageDialog(this,
+                "Add Product functionality will be implemented later",
+                "Add Prduct", JOptionPane.INFORMATION_MESSAGE);
+    }
+    private void editProductMessage() {
+        int selectedRow = productTable.getSelectedRow();
+        if (selectedRow == -1) return;
+
+        Product selectedProduct = tableModel.getProductAt(selectedRow);
+
+        // TODO: Make this edit product
+        // Placeholder for now
+        JOptionPane.showMessageDialog(this,
+                "Edit Product functionality will be implemented later",
+                "Edit Product", JOptionPane.INFORMATION_MESSAGE);
+    }
+    private void deleteProduct() {
 
     }
+
+
 
     private void loadProducts() {
 
